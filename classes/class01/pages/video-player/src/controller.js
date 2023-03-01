3043a4cacff9a4cca2ffce8c8beb8f5d005484a4
Matheus Carvalho <matheus.carvalho@zenvia.com>
2023-02-28 22:39:54 -0300

    init configure blick to play

--- classes/class01/pages/video-player/src/controller.js
+++ classes/class01/pages/video-player/src/controller.js
@@ -1,14 +1,36 @@
 export default class Controller {
-  constructor({}){
+  #view
+  #service 
+  #worker
+  constructor({view, service, worker}){
+    this.#view = view
+    this.#service = service
+    this.#worker = this.#configureWorker(worker)
 
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
   }
   static async initialize(deps){
     const controller = new Controller(deps)
+    controller.log('not yet detecting eye blick! click in the button to start')
     return controller.init()
 
   }
 
+  #configureWorker(worker){
+    worker.onmessage = (msg) =>{
+      console.log('recebi!')
+    }
+  }
+
   async init(){
     console.log('init!!!@@')
+  }
+
+  log(text){
+    this.#view.log(`logger:${text}`)
+  }
+
+  onBtnStart(){
+    this.log('initializing detection....')
   }
 }
--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -1,15 +1,33 @@
 import Camera from "../../../lib/shared/camera.js"
+import { supportsWorkerType } from "../../../lib/shared/util.js"
 import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
+
+async function getWorker(){
+  if(supportsWorkerType){
+    console.log('suporta')
+    const worker = new Worker('./src/worker.js',{type: 'module'})
+    return worker
+  }
+  const workerMock ={
+    async postMessage(){},
+    onmessage(msg){}
+  }
+   console.log('nao suporta')
+  return workerMock
+}
+
+const worker = await getWorker()
 
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
     return Controller.initialize({
-      view: new View({}),
-      service: new Service({})
+      view: new View(),
+      service: new Service({}),
+      worker
     })
   }
 }
